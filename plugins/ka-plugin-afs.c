2745b08d34f1bbb130abdd01e008c4f81c91efb9
Guido Günther <agx@sigxcpu.org>
2010-09-29 17:36:22 +0200

    New upstream version 0.17

--- plugins/ka-plugin-afs.c
+++ plugins/ka-plugin-afs.c
@@ -16,12 +16,12 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#include "ka-plugin-dummy.h"
+#include "ka-plugin-afs.h"
 #include <gmodule.h>
 
-G_DEFINE_TYPE (KaPluginDummy, ka_plugin_dummy, KA_TYPE_PLUGIN)
+G_DEFINE_TYPE (KaPluginAfs, ka_plugin_afs, KA_TYPE_PLUGIN)
 #define GET_PRIVATE(o) \
-  (G_TYPE_INSTANCE_GET_PRIVATE ((o), KA_TYPE_PLUGIN_DUMMY, KaPluginDummyPrivate))
+  (G_TYPE_INSTANCE_GET_PRIVATE ((o), KA_TYPE_PLUGIN_AFS, KaPluginAfsPrivate))
 
 int ka_plugin_major_version = KA_PLUGIN_MAJOR_VERSION;
 int ka_plugin_minor_version = KA_PLUGIN_MINOR_VERSION;
@@ -29,74 +29,86 @@ int ka_plugin_minor_version = KA_PLUGIN_MINOR_VERSION;
 G_MODULE_EXPORT KaPlugin *
 ka_plugin_create (void)
 {
-    return KA_PLUGIN (ka_plugin_dummy_new ());
+    return KA_PLUGIN (ka_plugin_afs_new ());
 }
 
-typedef struct _KaPluginDummyPrivate KaPluginDummyPrivate;
+typedef struct _KaPluginAfsPrivate KaPluginAfsPrivate;
 
-struct _KaPluginDummyPrivate {
-    gulong handlers[3];
+struct _KaPluginAfsPrivate {
+    gulong handlers[2];
 };
 
 static void
 event_cb (gpointer *applet, gchar *princ, guint when, gpointer user_data)
 {
-    g_message ("%s %s @%d", user_data, princ, when);
+    GError *err = NULL;
+    gboolean ret;
+    int i;
+    const char *afslog_cmds[] = { "aklog", "afslog" };
+
+    for (i = 0; i < G_N_ELEMENTS (afslog_cmds); i++) {
+        ret = g_spawn_command_line_async (afslog_cmds[i], &err);
+        if (!ret) {
+            if (G_SPAWN_ERROR_NOENT != err->code)
+                g_warning ("%s", err->message);
+        } else
+            break;
+        g_clear_error (&err);
+    }
+
+    if (!ret)
+        g_warning ("Couldn't run any afslog command");
 }
 
 static void
-ka_plugin_dummy_finalize (GObject *object)
+ka_plugin_afs_finalize (GObject *object)
 {
-    G_OBJECT_CLASS (ka_plugin_dummy_parent_class)->finalize (object);
+    G_OBJECT_CLASS (ka_plugin_afs_parent_class)->finalize (object);
 }
 
 static void
-ka_plugin_dummy_activate (KaPlugin *self, KaApplet *applet)
+ka_plugin_afs_activate (KaPlugin *self, KaApplet *applet)
 {
-    KaPluginDummyPrivate *priv = GET_PRIVATE (self);
+    KaPluginAfsPrivate *priv = GET_PRIVATE (self);
 
     priv->handlers[0] = g_signal_connect (applet,
-                                          "krb-tgt-expired",
-                                          G_CALLBACK (event_cb), "Expired");
-    priv->handlers[1] = g_signal_connect (applet,
                                           "krb-tgt-acquired",
                                           G_CALLBACK (event_cb), "Acquired");
-    priv->handlers[2] = g_signal_connect (applet,
+    priv->handlers[1] = g_signal_connect (applet,
                                           "krb-tgt-renewed",
                                           G_CALLBACK (event_cb), "Renewed");
 }
 
 static void
-ka_plugin_dummy_deactivate (KaPlugin *self, KaApplet *applet)
+ka_plugin_afs_deactivate (KaPlugin *self, KaApplet *applet)
 {
     int i;
-    KaPluginDummyPrivate *priv = GET_PRIVATE (self);
+    KaPluginAfsPrivate *priv = GET_PRIVATE (self);
 
     for (i = 0; i < G_N_ELEMENTS (priv->handlers); i++)
         g_signal_handler_disconnect (applet, priv->handlers[i]);
 }
 
 static void
-ka_plugin_dummy_class_init (KaPluginDummyClass *klass)
+ka_plugin_afs_class_init (KaPluginAfsClass *klass)
 {
     GObjectClass *object_class = G_OBJECT_CLASS (klass);
     KaPluginClass *plugin_class = KA_PLUGIN_CLASS (klass);
 
-    g_type_class_add_private (klass, sizeof (KaPluginDummyPrivate));
+    g_type_class_add_private (klass, sizeof (KaPluginAfsPrivate));
 
-    plugin_class->activate = ka_plugin_dummy_activate;
-    plugin_class->deactivate = ka_plugin_dummy_deactivate;
-    object_class->finalize = ka_plugin_dummy_finalize;
+    plugin_class->activate = ka_plugin_afs_activate;
+    plugin_class->deactivate = ka_plugin_afs_deactivate;
+    object_class->finalize = ka_plugin_afs_finalize;
 }
 
 static void
-ka_plugin_dummy_init (KaPluginDummy *self)
+ka_plugin_afs_init (KaPluginAfs *self)
 {
 }
 
-KaPluginDummy *
-ka_plugin_dummy_new (void)
+KaPluginAfs *
+ka_plugin_afs_new (void)
 {
-    return g_object_new (KA_TYPE_PLUGIN_DUMMY, KA_PLUGIN_PROP_NAME,
-                         "dummy", NULL);
+    return g_object_new (KA_TYPE_PLUGIN_AFS, KA_PLUGIN_PROP_NAME, "afs", NULL);
 }
